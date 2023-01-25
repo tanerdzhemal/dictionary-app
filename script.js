c28200ac984306541c9c380e9da7771e695ed21b
tanerdzhemal <123403094+tanerdzhemal@users.noreply.github.com>
2023-01-25 17:47:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = "https://api.dictionaryapi.dev/api/v2/entries/en/crazy";
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 
